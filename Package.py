aaf99b2001ffd778d551e01d335bac30fb8abb5b
Vivyen Vu <pokeviv@gmail.com>
2022-09-04 13:10:29 -0400

    Package class

---
+++ Package.py
@@ -0,0 +1,10 @@
+class Package:
+    def __init__(self, id, address, city, state, zipcode, deadline, weight, status):
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.deadline = deadline
+        self.weight = weight
+        self.status = status
