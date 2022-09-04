bd30211f0eb2ff417a9e61ade447b3a6531aa947
Vivyen Vu <pokeviv@gmail.com>
2022-09-04 13:01:36 -0400

    truck constructor

--- Truck.py
+++ Truck.py
@@ -1,1 +1,10 @@
 class Truck:
+    def __init__(self, name, maxPackages, speed, numPackages, mileage, currentAddress, departTime, currentTime):
+        self.name = name
+        self.maxPackages = maxPackages
+        self.speed = speed
+        self.numPackages = numPackages
+        self.mileage = mileage
+        self.currentAddress = currentAddress
+        self.departTime = departTime
+        self.currentTime = currentTime
