601d66663889a9f152d2f28c3eaf24d8467e1a19
Vivyen Vu <pokeviv@gmail.com>
2022-09-04 16:41:42 -0400

    comments for package_hashmap.py

--- package_hashmap.py
+++ package_hashmap.py
@@ -2,13 +2,17 @@ import csv
 from HashMap import HashMap
 from Package import Package
 
+# Create new hashmap to fill with package objects from Package.csv
 package_hashmap = HashMap()
 fileName = 'Package.csv'
 
 
+# Get contents from CSV file O(N^2)
 def csv_hashmap(fileName):
-    with open('fileName') as csvf:
+    with open(fileName) as csvf:
         contents = csv.reader(csvf)
+
+        # Create a package object from each entry O(N)
         for entry in contents:
             id = entry[0]
             address = entry[1]
@@ -17,7 +21,7 @@ def csv_hashmap(fileName):
             zipcode = entry[4]
             deadline = entry[5]
             weight = entry[6]
-
             newPackage = Package(id, address, city, state, zipcode, deadline, weight, 'In hub')
 
+            # Add package to hashmap
             package_hashmap.add(id, newPackage)
