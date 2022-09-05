4930d57c470f00f25095ed1b0555909aa80b4c77
Vivyen Vu <pokeviv@gmail.com>
2022-09-04 22:49:59 -0400

    notes for tomorrow

--- distance.py
+++ distance.py
@@ -4,6 +4,7 @@ import csv
 with open('Distance.csv') as csvf:
     distanceCSV = csv.reader(csvf)
     distanceCSV = list(distanceCSV)
+    # The csv. reader method returns a reader object which iterates over lines in the given CSV file. The numbers. csv file contains numbers
 
 def distance(a, b):
     distance = distanceCSV[a][b]
