f44e2c5790cd76c51239dee6cc9c639c847c23a7
Vivyen Vu <pokeviv@gmail.com>
2022-09-05 19:01:46 -0400

    figuring out IndexError in deliveryRoute

--- deliveryRoute.py
+++ deliveryRoute.py
@@ -10,28 +10,37 @@ csv_hashmap('Package.csv', package_hashmap)
 
 
 def goOnRoute(truck):
+    global removeIndex
     orderedRoute = [] # may not be necessary
     packageNumbers = truck.myPackages # List of package numbers
     packagesRemaining = len(packageNumbers)
 
     address1 = truck.currentAddress
     closestAddress = package_hashmap.lookup(packageNumbers[0]).address
-    print(closestAddress)
+    print(closestAddress) #test
+
+    print('DELIVERY ROUTE')
+    print(packageNumbers)
     while packagesRemaining > 0:
         for i in range(len(packageNumbers)):
             addressTemp = package_hashmap.lookup(packageNumbers[i]).address
             if distanceBetween(address1, addressTemp) < distanceBetween(address1, closestAddress):
                 address2 = addressTemp
                 nextPackage = package_hashmap.lookup(packageNumbers[i])
-                removeValue = i
+                removeThis= packageNumbers[i]
+                removeIndex = i
 
-        packagesRemaining -= 1
-        packageNumbers.remove(removeValue)
+        #packageNumbers.remove(removeThis)
+        print(removeIndex)
+        del packageNumbers[removeIndex]
+        print(packageNumbers)
         truck.updateTime(distanceBetween(address1, address2))
         truck.addMiles(distanceBetween(address1, address2))
         truck.updateAddress(address2)
-        package_hashmap.lookup(packageNumbers[removeValue]).deliveredAt(truck.currentTime)
-        package_hashmap.lookup(packageNumbers[removeValue]).deliveredStatus()
+        nextPackage.deliveredAt(truck.currentTime)
+        nextPackage.deliveredStatus()
+        address1 = address2
+        packagesRemaining -= 1
 
 
 
