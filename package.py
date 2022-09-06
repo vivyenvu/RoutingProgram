ce83b0c081e23851afff15a00fcf8e731e14cac2
Vivyen Vu <pokeviv@gmail.com>
2022-09-06 12:15:20 -0400

    package.py comments

--- package.py
+++ package.py
@@ -1,5 +1,6 @@
 # Creates class for package object
 class Package:
+    # Constructor where deliveredTime is None, but all other fields are inputted when this is called
     def __init__(self, id, address, city, state, zipcode, deadline, weight, status):
         self.id = id
         self.address = address
@@ -11,12 +12,18 @@ class Package:
         self.status = status
         self.deliveredTime = None
 
+    # Updates package's status to 'Delivered'
+    # Time complexity = O(1)
     def deliveredStatus(self):
         self.status = 'Delivered'
 
+    # Updates package's deliveredTime to the input time
+    # Time complexity = O(1)
     def deliveredAt(self, time):
         self.deliveredTime = time
 
+    # Updates package's status to 'On route'
+    # Time complexity = O(1)
     def onRoute(self):
         self.status = 'On route'
 
