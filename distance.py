77f4ef8d7ad55bae878ed5d312facd495f0f8f68
Vivyen Vu <pokeviv@gmail.com>
2022-09-06 12:13:09 -0400

    comments for distance.py and truck.py

--- distance.py
+++ distance.py
@@ -7,16 +7,20 @@ distanceDict = {}
 filename = 'Distance.csv'
 
 # Populate list with distances in between two addresses
+# Time complexity = O(n)
 with open(filename, encoding='utf-8-sig') as csvf:
     distanceCSV = csv.reader(csvf)
     for entry in distanceCSV:
         distancesBtwn.append(entry)
 
 # Create a dictionary with key: address index, and value: distances between that index and other address indexes
+# Time complexity = O(n)
 for x in range(0, len(distancesBtwn)):
     distanceDict[x] = distancesBtwn[x]
 
 
+# Look up the distance between two addresses
+# Time complexity = O(1)
 def distanceBetween(address1, address2):
     a1 = getAddressIndex(address1)
     a2 = getAddressIndex(address2)
--- truck.py
+++ truck.py
@@ -3,6 +3,8 @@ import datetime
 
 
 class Truck:
+    # Constructor for Truck where maxPackages, speed, mileage, and currentAddress are preset according to
+    # project details
     def __init__(self, myPackages, departTime):
         self.maxPackages = 16
         self.speed = 18
@@ -13,16 +15,15 @@ class Truck:
         self.currentTime = departTime
         self.route = []
 
+    # Sets truck's current address to a new address
     def updateAddress(self, newAddress):
         self.currentAddress = newAddress
 
+    # Calculates time elapsed to travel a certain amount of miles, and adds that to the truck's currentTime
     def updateTime(self, miles):
         elapsedTime = datetime.timedelta(seconds=((miles / 18) * 60 * 60))
         self.currentTime = self.currentTime + elapsedTime
 
+    # Adds input miles to truck's total mileage
     def addMiles(self, miles):
         self.mileage += miles
-
-    def setRoute(self, orderedRoute):
-        self.route = orderedRoute
-
