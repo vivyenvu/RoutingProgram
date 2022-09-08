d1daeeac5de2800bf89d3faccbeeadcce4cde9b4
Vivyen Vu <pokeviv@gmail.com>
2022-09-08 17:30:45 -0400

    minor update to hashMap

--- hashMap.py
+++ hashMap.py
@@ -23,14 +23,6 @@ class HashMap:
                     self.map[key_hash].append(key_value)
                     return True
 
-    def lookup(self, key):
-        key_hash = self.__get_hashindex(key)
-        if self.map[key_hash] is not None:
-            for pair in self.map[key_hash]:
-                if pair[0] == key:
-                    return pair[1]
-        return None
-
     def remove(self, key):
         key_hash = self.__get_hashindex(key)
 
@@ -40,4 +32,13 @@ class HashMap:
             if self.map[key_hash][i][0] == key:
                 self.map[key_hash].pop(i)
                 return True
+    def lookup(self, key):
+        key_hash = self.__get_hashindex(key)
+        if self.map[key_hash] is not None:
+            for pair in self.map[key_hash]:
+                if pair[0] == key:
+                    return pair[1]
+        return None
+
+
 
